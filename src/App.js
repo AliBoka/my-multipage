ebf4cf551cb003f2dea716f8b774aad114b57a6d
AliBoka <aliboka71@gmail.com>
2022-04-15 21:35:33 +0430

    feat : add home component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 function App() {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
+      <Home />
     </div>
   );
 }
