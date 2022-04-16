73fcca4daf82e3096e548eb366892f6a847aab92
Ali <aliboka71@gmail.com>
2022-04-17 03:17:51 +0430

    feat : add News component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import AboutUs from "./components/AboutUs/AboutUs";
 import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import News from "./components/News/News";
 function App() {
   return (
     <>
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/news" element={<News />} />
       </Routes>
      <Footer/>
 
