a9aae5d73e7dec27bb81b99246ee48e4d5cc4f5d
AliBoka <aliboka71@gmail.com>
2022-04-18 01:43:54 +0430

    feat : add the fourth home section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -17,6 +17,12 @@ import bigImg from "../assets/link (3) 1.png";
 import CyberImg from "../assets/cyber-security 1.png";
 import mobileAppImg from "../assets/coding 1.png";
 import digiImg from "../assets/promotion (1) 1.png";
+import bellAngleImg from '../assets/Bell Angle 1.png'
+import succlentImg from '../assets/Succlent angle 1.png'
+import vector1Img from '../assets/Vector 1.png'
+import vector2Img from '../assets/Vector 2.png'
+import vector3Img from '../assets/Vector 3.png'
+
 const Home = () => {
   const [starValue, setStarValue] = useState(4);
   const handleRating = (rate) => {
@@ -287,6 +293,31 @@ const Home = () => {
               </div>
             </div>
           </div>
+        </div>
+        <div className="w-full flex justify-center items-center h-[30rem] text-white bg-slate-100 ">
+            <div className="bg-primary px-24 lg:px-28 xl:px-40 2xl:px-44 py-24 flex flex-col md:flex-row items-center w-full justify-between relative">
+              <div className="flex flex-col items-center">
+                <h1 className="text-4xl lg:text-5xl xl:text-6xl font-medium">6</h1>
+                <p className="text-xs lg:text-sm xl:text-base">ppy Clients</p>
+              </div>
+              <div className="flex flex-col items-center">
+                <h1 className="text-4xl lg:text-5xl xl:text-6xl font-medium">11</h1>
+                <p className="text-xs lg:text-sm xl:text-base">Compled Projects</p>
+              </div>
+              <div className="flex flex-col items-center">
+                <h1 className="text-4xl lg:text-5xl xl:text-6xl font-medium">7M</h1>
+                <p className="text-xs lg:text-sm xl:text-base">anssactions</p>
+              </div>
+              <div className="flex flex-col items-center">
+                <h1 className="text-4xl lg:text-5xl xl:text-6xl font-medium">6000+</h1>
+                <p className="text-xs lg:text-sm xl:text-base">Customers</p>
+              </div>
+              <img src={bellAngleImg} alt="bellAngleImg" className="absolute w-24 -top-14 right-36 " />
+              <img src={succlentImg} alt="succlentImg" className="absolute w-20 -bottom-7 left-48 " />
+              <img src={vector1Img} alt="vector1Img" className="absolute w-32 bottom-0 left-0 " />
+              <img src={vector2Img} alt="vector2Img" className="absolute w-32 -top-16 left-24 " />
+              <img src={vector3Img} alt="vector3Img" className="absolute w-40 -top-8 right-44 " />
+            </div>
         </div>
       </div>
     </div>
