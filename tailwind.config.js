9fe9b87f04cbb530ade744903c44a964324f9a3d
AliBoka <aliboka71@gmail.com>
2022-04-17 19:01:06 +0430

    feat : (tailwind.config) : add some colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,13 @@ module.exports = {
       colors : {
         'primary' : '#070d18' ,
         'secondary' : '#41BBEA',
-        'rgba-white' : 'rgba(255, 255, 255, 0.29)'
+        'rgba-white' : 'rgba(255, 255, 255, 0.29)',
+        'rgba-white2' : 'rgba(255, 255, 255, 0.44)',
+        'rgba-white3' : 'rgba(255, 255, 255, 0.15)',
+        'rgba-white4' : 'rgba(255, 255, 255, 0.36)'
+      },
+      rotate : {
+        '360' : '360deg'
       }
     },
   },
