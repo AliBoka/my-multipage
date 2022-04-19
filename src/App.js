0ee6f04d4f98a727016f178840260f38bde7ad6c
AliBoka <aliboka71@gmail.com>
2022-04-20 01:17:42 +0430

    feat : add AboutUs component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import AboutUs from "./components/AboutUs/AboutUs";
 import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar";
 function App() {
   return (
     <>
+
+
       <Navbar />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about-us" element={<AboutUs />} />
+      </Routes>
+
+
     </>
-    
+
   );
 }
 
---
+++ src/components/AboutUs/AboutUs.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AboutUs = () => {
+  return (
+    <div className='h-screen bg-black'>AboutUs</div>
+  )
+}
+
+export default AboutUs
