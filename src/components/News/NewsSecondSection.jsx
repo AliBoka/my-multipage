89a8d0a992959596505cd774cce7a674a2197c13
AliBoka <aliboka71@gmail.com>
2022-04-21 17:08:01 +0430

    fix (NewsSecond): fix a responsive bug a component

--- src/components/News/NewsSecondSection.jsx
+++ src/components/News/NewsSecondSection.jsx
@@ -167,8 +167,8 @@ const NewsSecondSection = () => {
       </div>
       <div className="w-full flex justify-center items-center mx-auto my-20 pt-8 pb-24">
 
-      <div className="w-full flex justify-between items-center bg-gray-300 px-8 py-7 rounded-md">
-        <div className="flex flex-col w-1/2">
+      <div className="w-full flex flex-col sm:flex-row justify-between items-center bg-gray-300 px-8 py-7 rounded-md">
+        <div className="flex flex-col sm:w-1/2">
           <h1 className="text-xl font-medium">Stay in the loop</h1>
           <p className="text-gray-600 text-sm">
             Subscribe to receive the latest news and updates about TDA. We
@@ -179,7 +179,7 @@ const NewsSecondSection = () => {
           <div className=" w-full flex justify-center"> 
           <div className="relative">
 
-          <input type="email" className="bg-white text-sm pl-3 pr-[6.5rem] py-2   w-72 rounded placeholder:text-gray-600" placeholder="Enter email address" />
+          <input type="email" className="bg-white text-sm pl-3 pr-[6.5rem] py-2   w-64 sm:w-72 mt-10 sm:mt-0 rounded placeholder:text-gray-600" placeholder="Enter email address" />
           <button className="absolute bg-secondary text-white rounded px-3 py-1.5 text-sm flex justify-center items-center right-0.5 bottom-0.5">Continue</button>
           </div>
 
