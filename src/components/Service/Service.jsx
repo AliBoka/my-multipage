d9f2885d4a94945c5c773758938f59537a5e9138
AliBoka <aliboka71@gmail.com>
2022-04-21 19:27:36 +0430

    feat : add code to service component

--- src/components/Service/Service.jsx
+++ src/components/Service/Service.jsx
@@ -1,9 +1,219 @@
-import React from 'react'
+import React from "react";
+import postImg1 from "../../assets/Rectangle 9.png";
+import postImg2 from "../../assets/Rectangle 10-1.png";
+import postImg3 from "../../assets/Rectangle 11-1.png";
+import postImg4 from "../../assets/Rectangle 10-2.png";
+import postImg5 from "../../assets/Rectangle 10-3.png";
+import postImg8 from "../../assets/Rectangle 10.png";
+import postImg9 from "../../assets/Rectangle 11.png";
+import postImg10 from "../../assets/Rectangle 11 (1).png";
+
 
 const Service = () => {
   return (
-    <div>Service</div>
-  )
-}
+    <div className="flex flex-col w-full pt-32 px-12 ">
+      <h1 className="text-2xl lg:text-3xl font-semibold mb-7">
+        Our <span className="text-secondary">Service</span>
+      </h1>
 
-export default Service
+      <div className="flex flex-col md:flex-row w-full items-center md:items-stretch mb-32">
+        <div className="md:w-1/2">
+          <img src={postImg1} alt="postImg1" className="h-full xl:ml-11" />
+        </div>
+        <div className="flex flex-col   md:w-1/2  px-4 justify-between  lg:px-14 mt-7 md:mt-0">
+          <div className="flex justify-center flex-col  ">
+            <h1 className="text-xl font-medium mb-5 xl:text-2xl xl:font-semibold xl:pr-5 ">
+              1. Big Data & Business Analytics
+            </h1>
+            <p className="text-gray-700 text-sm xl:text-base   lg:pr-8 xl:pr-28">
+              Data is your most valuable asset.But only if you make the most of
+              it. AFQ Tech data scientists and analysts are skilled at mining,
+              modeling, and extracting value from data. Sourced from a range of
+              sectors and with hands-on experience, they put you in charge and
+              draw on the best knowledge and expertise to help you navigate your
+              way forward.
+            </p>
+          </div>
+
+          <div className="flex justify-center items-center text-white w-32 h-11 rounded mt-4 bg-primary cursor-pointer hover:bg-secondary">
+            Read more
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col-reverse  md:flex-row w-full items-center md:items-stretch mb-32">
+        <div className="flex flex-col   md:w-1/2  px-4 justify-between  lg:px-14 mt-7 md:mt-0">
+          <div className="flex justify-center flex-col  ">
+            <h1 className="text-xl font-medium mb-5 xl:text-2xl xl:font-semibold xl:pr-5 ">
+              2. IoT/ AI/ Robotics
+            </h1>
+            <p className="text-gray-700 text-sm xl:text-base   lg:pr-8 xl:pr-28">
+              From automation to advanced analytics and seamless experiences, we
+              can embed AI in business. We'll deliver new operating models and
+              strategic intelligence for smart processes and data-driven
+              decisions.
+              <br /> <br />
+              What, Why and How?
+            </p>
+          </div>
+
+          <div className="flex justify-center items-center text-white w-32 h-11 rounded mt-4 bg-primary cursor-pointer hover:bg-secondary">
+            Read more
+          </div>
+        </div>
+        <div className="md:w-1/2 flex">
+          <img src={postImg8} alt="postImg8" className="h-full xl:ml-11" />
+        </div>
+      </div>
+      <div className="flex flex-col md:flex-row w-full items-center md:items-stretch mb-32">
+        <div className="md:w-1/2">
+          <img src={postImg10} alt="postImg10" className="h-full xl:ml-11" />
+        </div>
+        <div className="flex flex-col   md:w-1/2  px-4 justify-between  lg:px-14 mt-7 md:mt-0">
+          <div className="flex justify-center flex-col  ">
+            <h1 className="text-xl font-medium mb-5 xl:text-2xl xl:font-semibold xl:pr-5 ">
+              1. Big Data & Business Analytics
+            </h1>
+            <p className="text-gray-700 text-sm xl:text-base   lg:pr-8 xl:pr-28">
+              Data is your most valuable asset.But only if you make the most of
+              it. AFQ Tech data scientists and analysts are skilled at mining,
+              modeling, and extracting value from data. Sourced from a range of
+              sectors and with hands-on experience, they put you in charge and
+              draw on the best knowledge and expertise to help you navigate your
+              way forward.
+            </p>
+          </div>
+
+          <div className="flex justify-center items-center text-white w-32 h-11 rounded mt-4 bg-primary cursor-pointer hover:bg-secondary">
+            Read more
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col-reverse  md:flex-row w-full items-center md:items-stretch mb-32">
+        <div className="flex flex-col   md:w-1/2  px-4 justify-between  lg:px-14 mt-7 md:mt-0">
+          <div className="flex justify-center flex-col  ">
+            <h1 className="text-xl font-medium mb-5 xl:text-2xl xl:font-semibold xl:pr-5 ">
+              2. IoT/ AI/ Robotics
+            </h1>
+            <p className="text-gray-700 text-sm xl:text-base   lg:pr-8 xl:pr-28">
+              From automation to advanced analytics and seamless experiences, we
+              can embed AI in business. We'll deliver new operating models and
+              strategic intelligence for smart processes and data-driven
+              decisions.
+              <br /> <br />
+              What, Why and How?
+            </p>
+          </div>
+
+          <div className="flex justify-center items-center text-white w-32 h-11 rounded mt-4 bg-primary cursor-pointer hover:bg-secondary">
+            Read more
+          </div>
+        </div>
+        <div className="md:w-1/2 flex">
+          <img src={postImg2} alt="postImg2" className="h-full xl:ml-11" />
+        </div>
+      </div>
+      <div className="flex flex-col md:flex-row w-full items-center md:items-stretch mb-32">
+        <div className="md:w-1/2">
+          <img src={postImg9} alt="postImg9" className="h-full xl:ml-11" />
+        </div>
+        <div className="flex flex-col   md:w-1/2  px-4 justify-between  lg:px-14 mt-7 md:mt-0">
+          <div className="flex justify-center flex-col  ">
+            <h1 className="text-xl font-medium mb-5 xl:text-2xl xl:font-semibold xl:pr-5 ">
+              1. Big Data & Business Analytics
+            </h1>
+            <p className="text-gray-700 text-sm xl:text-base   lg:pr-8 xl:pr-28">
+              Data is your most valuable asset.But only if you make the most of
+              it. AFQ Tech data scientists and analysts are skilled at mining,
+              modeling, and extracting value from data. Sourced from a range of
+              sectors and with hands-on experience, they put you in charge and
+              draw on the best knowledge and expertise to help you navigate your
+              way forward.
+            </p>
+          </div>
+
+          <div className="flex justify-center items-center text-white w-32 h-11 rounded mt-4 bg-primary cursor-pointer hover:bg-secondary">
+            Read more
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col-reverse  md:flex-row w-full items-center md:items-stretch mb-32">
+        <div className="flex flex-col   md:w-1/2  px-4 justify-between  lg:px-14 mt-7 md:mt-0">
+          <div className="flex justify-center flex-col  ">
+            <h1 className="text-xl font-medium mb-5 xl:text-2xl xl:font-semibold xl:pr-5 ">
+              2. IoT/ AI/ Robotics
+            </h1>
+            <p className="text-gray-700 text-sm xl:text-base   lg:pr-8 xl:pr-28">
+              From automation to advanced analytics and seamless experiences, we
+              can embed AI in business. We'll deliver new operating models and
+              strategic intelligence for smart processes and data-driven
+              decisions.
+              <br /> <br />
+              What, Why and How?
+            </p>
+          </div>
+
+          <div className="flex justify-center items-center text-white w-32 h-11 rounded mt-4 bg-primary cursor-pointer hover:bg-secondary">
+            Read more
+          </div>
+        </div>
+        <div className="md:w-1/2 flex">
+          <img src={postImg4} alt="postImg4" className="h-full xl:ml-11" />
+        </div>
+      </div>
+      <div className="flex flex-col md:flex-row w-full items-center md:items-stretch mb-32">
+        <div className="md:w-1/2">
+          <img src={postImg3} alt="postImg1" className="h-full xl:ml-11" />
+        </div>
+        <div className="flex flex-col   md:w-1/2  px-4 justify-between  lg:px-14 mt-7 md:mt-0">
+          <div className="flex justify-center flex-col  ">
+            <h1 className="text-xl font-medium mb-5 xl:text-2xl xl:font-semibold xl:pr-5 ">
+              1. Big Data & Business Analytics
+            </h1>
+            <p className="text-gray-700 text-sm xl:text-base   lg:pr-8 xl:pr-28">
+              Data is your most valuable asset.But only if you make the most of
+              it. AFQ Tech data scientists and analysts are skilled at mining,
+              modeling, and extracting value from data. Sourced from a range of
+              sectors and with hands-on experience, they put you in charge and
+              draw on the best knowledge and expertise to help you navigate your
+              way forward.
+            </p>
+          </div>
+
+          <div className="flex justify-center items-center text-white w-32 h-11 rounded mt-4 bg-primary cursor-pointer hover:bg-secondary">
+            Read more
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col-reverse  md:flex-row w-full items-center md:items-stretch mb-32">
+        <div className="flex flex-col   md:w-1/2  px-4 justify-between  lg:px-14 mt-7 md:mt-0">
+          <div className="flex justify-center flex-col  ">
+            <h1 className="text-xl font-medium mb-5 xl:text-2xl xl:font-semibold xl:pr-5 ">
+              2. IoT/ AI/ Robotics
+            </h1>
+            <p className="text-gray-700 text-sm xl:text-base   lg:pr-8 xl:pr-28">
+              From automation to advanced analytics and seamless experiences, we
+              can embed AI in business. We'll deliver new operating models and
+              strategic intelligence for smart processes and data-driven
+              decisions.
+              <br /> <br />
+              What, Why and How?
+            </p>
+          </div>
+
+          <div className="flex justify-center items-center text-white w-32 h-11 rounded mt-4 bg-primary cursor-pointer hover:bg-secondary">
+            Read more
+          </div>
+        </div>
+        <div className="md:w-1/2 flex">
+          <img src={postImg5} alt="postImg5" className="h-full xl:ml-11" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Service;
